01fcccf6f3e742700d90e01a9b684f5ea57eb0e5
Marco Sehrer <ms@ninjaconcept.com>
2010-02-23 12:15:48 +0100

    merge newest dry_generator files

--- generators/dry_model/dry_model_generator.rb
+++ generators/dry_model/dry_model_generator.rb
@@ -19,7 +19,8 @@ class DryModelGenerator < DryGenerator
               :references
               
   def initialize(runtime_args, runtime_options = {})
-    super(runtime_args, runtime_options)
+    @options = DEFAULT_OPTIONS.merge(options)
+    super(runtime_args, runtime_options.merge(@options))
     
     @attributes ||= []
     args_for_model = []
@@ -45,7 +46,6 @@ class DryModelGenerator < DryGenerator
     
     @args = args_for_model
     @references = attributes.select(&:reference?)
-    @options = DEFAULT_OPTIONS.merge(options)
   end
   
   def manifest
